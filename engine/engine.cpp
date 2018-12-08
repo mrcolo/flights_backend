d703284e8bee5e6271fcae826c9eb4520158bcfa
mrcolo <colo28197@gmail.com>
2018-12-08 11:31:43 -0800

    works in server as well

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -113,12 +113,12 @@ std::string engine::getAirports() {
 
 void engine::openAirports(std::ifstream& i){
 //WHEN EXECUTING FROM MAIN    i.open("../data/airports_iata.csv");
-/*WHEN EXECUTING FROM WEBSERVER*/    i.open("../data/airports_iata.csv");
+/*WHEN EXECUTING FROM WEBSERVER*/    i.open("./data/airports_iata.csv");
 }
 
 void engine::openRoutes(std::ifstream& i) {
     //WHEN EXECUTING FROM MAIN    i.open("../data/routes_final.csv");
-/*WHEN EXECUTING FROM WEBSERVER*/    i.open("../data/routes_final.csv");
+/*WHEN EXECUTING FROM WEBSERVER*/    i.open("./data/routes_final.csv");
 }
 
 void engine::loadAirports(std::vector<std::vector<std::string>>& airports) {
